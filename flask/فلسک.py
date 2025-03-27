06169310ffc5126720764aa3a92cf156b62fc1d6
aminkoozeh <aminkoozehkanay@gmail.com>
2025-03-27 14:21:46 +0330

    Update فلسک.py

--- flask/فلسک.py
+++ flask/فلسک.py
@@ -196,4 +196,5 @@ def salam():
 #    vorody = request.form['user']
 #    return f"نام شما:{vorody}"
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
